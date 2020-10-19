82e4bba0536ea91fbca6d1ba99fd25e47104af21
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2020-10-19 18:44:51 +0900

    Dev スクロール促進機能の動作を調整

--- spotlight.js
+++ spotlight.js
@@ -28,13 +28,13 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!isHidden) {
       document.getElementById("dark-over").classList.add('hide_dark-over');
       setTimeout(() => isHidden = true, 1000);
-      document.getElementById('hero').classList.add('isMinimized', 'isAnimating');
+      setTimeout(() => document.getElementById('hero').classList.add('isMinimized', 'isAnimating'), 500);
       setTimeout(() => {
         let evt = document.createEvent("HTMLEvents");
         evt.initEvent('scroll', true, true);
         document.body.dispatchEvent(evt);
         document.getElementById('hero').classList.remove('isAnimating');
-      }, 2000);
+      }, 2500);
     }
   }, 5000);
 
